04cb1f3d6ddee829a19c9db8bfa2804a1341d73c
maneesha <maneeshalakshani9@gmail.com>
2021-09-17 00:14:14 +0530

    Test commit in master

--- app/src/main/java/com/example/glimmerthreads/MainActivity.java
+++ app/src/main/java/com/example/glimmerthreads/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static java.sql.DriverManager.println;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        println("Hello this line is for pushing to gitgub test");
     }
 }
